ca5c37bcda95ff7f0bfbd54fc336796fb596a03a
Prabhat789 <prabhat.kumar.ec@gmail.com>
2016-03-24 14:15:04 +0530

    Added HomeScreen

---
+++ app/src/main/java/com/pktworld/railway/activity/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.pktworld.railway.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.pktworld.railway.R;
+
+/**
+ * Created by ubuntu1 on 24/3/16.
+ */
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
