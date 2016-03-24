2aea27fcb988de946a1481e98f64e8762e2ddc58
Prabhat <prabhat.kumar.ec@gmail.com>
2016-03-25 02:08:43 +0530

    home screen added

--- app/src/main/java/com/pktworld/railway/fragments/FragmentInHome.java
+++ app/src/main/java/com/pktworld/railway/fragments/FragmentInHome.java
@@ -9,16 +9,18 @@ import android.view.ViewGroup;
 import com.pktworld.railway.R;
 
 /**
- * Created by ubuntu1 on 24/3/16.
+ * Created by Prabhat on 25/03/16.
  */
-public class ChatRoomFragment extends Fragment {
+public class FragmentInHome extends Fragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_char_room, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_in_home, container, false);
 
         return rootView;
 
     }
+
+
 }
