841d74bf7fc8f4524432382bd61b93f1962ca7a8
Prabhat <prabhat.kumar.ec@gmail.com>
2016-05-20 11:57:08 +0530

    Added Cancelled Train

--- app/src/main/java/com/pktworld/railway/fragments/ChatRoomFragment.java
+++ app/src/main/java/com/pktworld/railway/fragments/ChatRoomFragment.java
@@ -1,13 +1,10 @@
 package com.pktworld.railway.fragments;
 
 import android.app.ProgressDialog;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -24,21 +21,19 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.pktworld.railway.R;
+import com.pktworld.railway.model.PnrResponse;
 import com.pktworld.railway.parseutils.Group;
-import com.pktworld.railway.parseutils.GroupListAdapter;
 import com.pktworld.railway.parseutils.OneToOneChatActivity;
 import com.pktworld.railway.parseutils.SpacesItemDecoration;
 import com.pktworld.railway.util.ApplicationConstants;
+import com.pktworld.railway.util.GsonRequestResponseHelper;
 import com.pktworld.railway.util.Utils;
-
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -54,7 +49,7 @@ public class ChatRoomFragment extends Fragment implements View.OnClickListener{
     private RecyclerView mRecyclerView;
     private RecyclerView.LayoutManager mLayoutManager;
     private ArrayList<Group> group;
-    private GroupListAdapter mAdapter;
+   // private GroupListAdapter mAdapter;
     private Button btnSearchGroup,btnCreateGroup;
     private RequestQueue mRequestQueue;
     private TextView txtTrainName;
@@ -65,8 +60,8 @@ public class ChatRoomFragment extends Fragment implements View.OnClickListener{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_char_room, container, false);
-        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(mMessageReceiver,
-                new IntentFilter(ApplicationConstants.GROUP_LIST_ADAPTER));
+        /*LocalBroadcastManager.getInstance(getActivity()).registerReceiver(mMessageReceiver,
+                new IntentFilter(ApplicationConstants.GROUP_LIST_ADAPTER));*/
 
         mRecyclerView = (RecyclerView)rootView.findViewById(R.id.listGroup);
         mRecyclerView.setHasFixedSize(true);
@@ -91,7 +86,7 @@ public class ChatRoomFragment extends Fragment implements View.OnClickListener{
 
     }
 
-    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+   /* private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             String userId = intent.getStringExtra(ApplicationConstants.FLAG1);
@@ -104,29 +99,44 @@ public class ChatRoomFragment extends Fragment implements View.OnClickListener{
             i.putExtra(ApplicationConstants.FLAG3,gName);
             startActivity(i);
         }
-    };
+    };*/
 
 
    public void SearchGroup(final String q){
+       mProgressDialog = ProgressDialog.show(getActivity(), "",
+               getResources().getString(R.string.processing), true);
+       mProgressDialog.show();
         ParseQuery<Group> query = ParseQuery.getQuery(Group.class);
-       query.whereContains("groupName",q);
+        query.whereContains("groupName",q);
         query.setLimit(1);
         query.findInBackground(new FindCallback<Group>() {
             public void done(List<Group> messages, ParseException e) {
+                if (mProgressDialog.isShowing()){
+                    mProgressDialog.dismiss();
+                }
                 if (e == null) {
                     group.clear();
                     Collections.reverse(messages);
                     group.addAll(messages);
-                    mAdapter.notifyDataSetChanged();
-                    mRecyclerView.invalidate();
+                    /*mAdapter.notifyDataSetChanged();
+                    mRecyclerView.invalidate();*/
 
                     if (messages.size() == 0){
-                        txtTrainName.setText(q);
+                        /*txtTrainName.setText(q);
                         layoutNoGroup.setVisibility(View.VISIBLE);
-                        layoutRecyclerList.setVisibility(View.GONE);
+                        layoutRecyclerList.setVisibility(View.GONE);*/
+                        createGroup(q,editTrainNumber.getText().toString());
                     }else {
-                        layoutRecyclerList.setVisibility(View.VISIBLE);
-                        layoutNoGroup.setVisibility(View.GONE);
+                        /*layoutRecyclerList.setVisibility(View.VISIBLE);
+                        layoutNoGroup.setVisibility(View.GONE);*/
+                        /*String userId = intent.getStringExtra(ApplicationConstants.FLAG1);
+                        String userName = intent.getStringExtra(ApplicationConstants.FLAG2);
+                        String gName = intent.getStringExtra(ApplicationConstants.FLAG3);*/
+                        Intent i = new Intent(getActivity(), OneToOneChatActivity.class);
+                        i.putExtra(ApplicationConstants.FLAG1, group.get(0).getObjectId());
+                        i.putExtra(ApplicationConstants.FLAG2, group.get(0).getGroupName());
+                        i.putExtra(ApplicationConstants.FLAG3,group.get(0).getGroupName());
+                        startActivity(i);
                     }
 
                 } else {
@@ -140,9 +150,10 @@ public class ChatRoomFragment extends Fragment implements View.OnClickListener{
     public void onClick(View v) {
         if (v == btnSearchGroup){
             if (editTrainNumber.getText().toString().length() !=0 || !editTrainNumber.getText().toString().isEmpty()){
-                getTrainName(getActivity());
+                //getTrainName(getActivity());
+                getPnrStatus(getActivity());
             }else {
-                Utils.showToastMessage(getActivity(),"Please enter Train Number");
+                Utils.showToastMessage(getActivity(),"Please enter your PNR Number");
             }
         }else if (v == btnCreateGroup){
             if (editPnrNumber.getText().toString().length() !=0 || !editPnrNumber.getText().toString().isEmpty()){
@@ -153,7 +164,7 @@ public class ChatRoomFragment extends Fragment implements View.OnClickListener{
         }
     }
 
-    private void getTrainName(Context mContext) {
+   /* private void getTrainName(Context mContext) {
         mProgressDialog = ProgressDialog.show(mContext, "",
                 getResources().getString(R.string.processing), true);
         mProgressDialog.show();
@@ -178,7 +189,7 @@ public class ChatRoomFragment extends Fragment implements View.OnClickListener{
                                 if (mProgressDialog.isShowing()){
                                     mProgressDialog.dismiss();
                                 }
-                                SearchGroup(train.getString("number")+" "+train.getString("name"));
+                               // SearchGroup(train.getString("number")+" "+train.getString("name"));
                                 mAdapter = new GroupListAdapter(getActivity(), group);
                                 mRecyclerView.setAdapter(mAdapter);
 
@@ -212,19 +223,25 @@ public class ChatRoomFragment extends Fragment implements View.OnClickListener{
                 DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
                 DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         mRequestQueue.add(stringRequest);
-    }
+    }*/
 
     void createGroup(final String trainNumber, String pnrNumber){
+        mProgressDialog = ProgressDialog.show(getActivity(), "",
+                getResources().getString(R.string.processing), true);
+        mProgressDialog.show();
         Group message = new Group();
         message.setGroupName(trainNumber);
         message.setPnr(pnrNumber);
         message.saveInBackground(new SaveCallback() {
             @Override
             public void done(ParseException e) {
+                if (mProgressDialog.isShowing()){
+                    mProgressDialog.dismiss();
+                }
                 if (e == null) {
                     SearchGroup(trainNumber);
-                    mAdapter = new GroupListAdapter(getActivity(), group);
-                    mRecyclerView.setAdapter(mAdapter);
+                   /* mAdapter = new GroupListAdapter(getActivity(), group);
+                    mRecyclerView.setAdapter(mAdapter);*/
                 }
 
             }
@@ -233,7 +250,86 @@ public class ChatRoomFragment extends Fragment implements View.OnClickListener{
 
     @Override
     public void onDestroy() {
-        LocalBroadcastManager.getInstance(getActivity()).unregisterReceiver(mMessageReceiver);
+       // LocalBroadcastManager.getInstance(getActivity()).unregisterReceiver(mMessageReceiver);
         super.onDestroy();
+    }
+
+    private void getPnrStatus(Context mContext){
+        mProgressDialog = ProgressDialog.show(mContext, "",
+                getResources().getString(R.string.processing), true);
+        mProgressDialog.show();
+        //"http://api.railwayapi.com/pnr_status/pnr/1234567890/apikey/myapikey/"
+        String REQUEST_URL = ApplicationConstants.RAILWAY_API_URL+"pnr_status/pnr/"+
+                editTrainNumber.getText().toString().trim()+"/apikey/"+ApplicationConstants.RAILWAY_API_KEY;
+
+        mRequestQueue = Volley.newRequestQueue(mContext);
+
+        // Request with API parameters
+        GsonRequestResponseHelper<PnrResponse> myReq = new GsonRequestResponseHelper<PnrResponse>(
+                Request.Method.GET,
+                REQUEST_URL,
+                PnrResponse.class,
+                createMyReqSuccessListener(),
+                createMyReqErrorListener());
+
+        myReq.setRetryPolicy(new DefaultRetryPolicy(
+                ApplicationConstants.MY_SOCKET_TIMEOUT_MS,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        mRequestQueue.add(myReq);
+    }
+
+    private Response.Listener<PnrResponse> createMyReqSuccessListener() {
+        return new Response.Listener<PnrResponse>() {
+            @Override
+            public void onResponse(PnrResponse response) {
+                try {
+                    if (mProgressDialog.isShowing()){
+                        mProgressDialog.dismiss();
+                    }
+                    if (response.getResponse_code().equals("200")){
+                        String trainName = response.getTrain_name();
+                        String txtTrainNumber = response.getTrain_num();
+                        String chartPrepared = response.getChart_prepared();
+                        if (chartPrepared.equalsIgnoreCase("Y")){
+                            SearchGroup(txtTrainNumber+" "+trainName);
+                        }
+                        /*txtPnr.setText("PNR : "+response.getPnr());
+                        txtDoj.setText("Date Of Journey : "+response.getDoj());
+                        txtChartPrep.setText("Chart Prepared : "+response.getChart_prepared());
+                        txtTotalPass.setText("Total Passengers : "+response.getTotal_passengers());
+                        txtTrainStartDate.setText("Start Date : "+response.getTrain_start_date().getDay()+"/"+
+                                response.getTrain_start_date().getMonth()+"/"+response.getTrain_start_date().getYear());
+                        txtFromStation.setText("From Station : "+response.getFrom_station().getName()+" ["+response.getFrom_station().getCode()+"]");
+                        txtToStation.setText("To Station : "+response.getTo_station().getName()+" ["+response.getTo_station().getCode()+"]");
+
+                        mAdapers = new PassangerAdapter(PnrStatusActivity.this,response.getPassengers());
+                        mList.setAdapter(mAdapers);*/
+
+
+                    }else {
+                        Utils.showToastMessage(getActivity(),response.getError());
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    if (mProgressDialog.isShowing()){
+                        mProgressDialog.dismiss();
+                    }
+                    Log.e(TAG, "TryCatch");
+                }
+            };
+        };
+    }
+
+    private Response.ErrorListener createMyReqErrorListener() {
+        return new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if (mProgressDialog.isShowing()){
+                    mProgressDialog.dismiss();
+                } Log.e(TAG, "Error");
+            }
+        };
     }
 }
